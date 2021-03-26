d990636842dd88073a747a674befc2732e8ec378
Ines L <inesluparia@gmail.com>
2021-03-26 15:08:47 +0100

    Initial commit

---
+++ src/com/kea/Main.java
@@ -0,0 +1,9 @@
+package com.kea;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Trying to deploy this java app in aws! -is it working???");
+    }
+}
